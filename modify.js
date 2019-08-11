4475e86058e343fdf7348a4ca84e37fe65ed74f9
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-08-11 13:23:27 -0500

    Changed comment wording in modify.js

--- modify.js
+++ modify.js
@@ -1,15 +1,9 @@
+// First, we have to grab all of the stuff we need to make this file work
 import Phaser from "phaser";
-
 import Character from "/.DO_NOT_TOUCH/classes/Character.js";
 
-console.log("it worked");
-
-// This function creates a player and adds it to the scene
-export function createPlayer(scene) {
-  console.log("creating player");
-  this.johnny = new Character(this, 10, 0);
-  this.johnny.sprite.setCollideWorldBounds(true);
-}
+/***********************************************************
+ *  Do Stuff after this line! */
 
 // This function creates platforms and adds them to the scene
 export function createPlatforms(scene) {
@@ -33,4 +27,11 @@ export function createPlatforms(scene) {
 // This function creates a goal and adds it to the scene
 export function createGoal(scene) {
   // Goal
+}
+
+// This function creates a player and adds it to the scene
+export function createPlayer(scene) {
+  console.log("creating player");
+  this.johnny = new Character(this, 10, 0);
+  this.johnny.sprite.setCollideWorldBounds(true);
 }
